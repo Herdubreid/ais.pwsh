2ebd90c02211ec54ea9e534c8750183251573008
Finnur Bragason <fbragason@outlook.com>
2023-08-15 08:51:07 +0800

    .

---
+++ scripts/using.ps1
@@ -0,0 +1,32 @@
+function using
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyString()]
+        [AllowEmptyCollection()]
+        [AllowNull()]
+        [Object]
+        $InputObject,
+
+        [Parameter(Mandatory = $true)]
+        [scriptblock]
+        $ScriptBlock
+    )
+
+    try
+    {
+        . $ScriptBlock
+    }
+    finally
+    {
+        if ($null -ne $InputObject -and $InputObject -is [System.IDisposable])
+        {
+            $InputObject.Dispose()
+        }
+    }
+}
+# Sample
+# using ($streamWriter = New-Object System.IO.StreamWriter("$pwd\newfile.txt")) {
+#  $streamWriter.WriteLine('Line written inside Using block.')
+#}
