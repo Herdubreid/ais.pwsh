05fafd6160d3956802674b4bdb04aebb7ad31791
Finnur Bragason <fbragason@outlook.com>
2023-08-31 08:05:38 +0800

    .

---
+++ scripts/get-layout.ps1
@@ -0,0 +1,7 @@
+
+function get-layout {
+    param([string] $name)
+    $rq = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Herdubreid/ais.pwsh/main/layouts/${name}.json"
+    return $rq.content
+  }
+  
