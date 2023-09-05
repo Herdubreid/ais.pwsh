49e6efd61ab4c895f650317200b1ab9d0915351a
Finnur Bragason <fbragason@outlook.com>
2023-09-05 13:15:30 +0800

    .

--- scripts/item-query.ps1
+++ scripts/item-query.ps1
@@ -1,6 +1,6 @@
 # Init
 . getScript qop, jde-form, jde-grid-form, w4101e
-    
+
 set-celin.ais.ui main (getLayout dark)
 $search = convertFrom-celin.ais.ui.form (getLayout item-search)
 $select = convertfrom-celin.ais.ui.gridform (getLayout item-select)
@@ -9,7 +9,7 @@ do {
     remove-variable flt, item -errorAction silentlycontinue
     $flt = show-celin.ais.ui.form $search
     if ($flt) {
-        [w4101e]::open($flt)        
+        [w4101e]::open($flt.data)
         $select.body.data = $jde.grid.detail
         $item = show-celin.ais.ui.gridform $select
         $jde.exit()
