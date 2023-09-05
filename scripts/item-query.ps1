fbb64dbb2be86a3072fe6faa04ad9fea67770294
Finnur Bragason <fbragason@outlook.com>
2023-09-05 13:01:02 +0800

    .

--- scripts/item-query.ps1
+++ scripts/item-query.ps1
@@ -1,12 +1,12 @@
 # Init
-. getScript jde-form,jde-grid-form,qop
+. getScript qop, jde-form, jde-grid-form, w4101e
     
 set-celin.ais.ui main (getLayout dark)
 $search = convertFrom-celin.ais.ui.form (getLayout item-search)
 $select = convertfrom-celin.ais.ui.gridform (getLayout item-select)
 
 do {
-    remove-variable flt,item -errorAction silentlycontinue
+    remove-variable flt, item -errorAction silentlycontinue
     $flt = show-celin.ais.ui.form $search
     if ($flt) {
         [w4101e]::open($flt)        
@@ -14,4 +14,4 @@ do {
         $item = show-celin.ais.ui.gridform $select
         $jde.exit()
     }
-} until (-not $item)
+} until ($item.key -ne "S")
---
+++ scripts/w4101e.ps1
@@ -0,0 +1,42 @@
+# Menu 4, Select
+# Menu 22, Find
+# Menu 17, Add
+# Menu 75, Copy
+# Menu 19, Delete
+# Menu 5, Close
+# Form 129, Item Word Search
+# Row 153, Item Features
+# Row 132, Item Revisions
+# Row 133, Category Codes
+# Row 134, Addl System Info
+# Row 135, Storage/Shipping
+# Row 139, Cost Revisions
+# Row 69, Price Revisions
+# Row 71, Item Availability
+# Row 72, Item Branch
+# Row 108, Item Notes
+# Row 70, Internal Attachment
+# Row 77, Item Alt Desc
+# Row 137, Segmented Itm Avai
+# Row 124, Product Mix
+# Row 130, Regional Info
+
+class w4101e : jdeGridForm { 
+    w4101e($rs) : base($rs) {}
+    [void] select([int]$row) {
+        $rs = step.celin.ais.script "select(1.$row) do(4)"
+        [jdeForm]::update($rs)
+    }
+    static open($flt) {
+        $q = "all($(qop "1[123]" "?" $flt[0].value.toupper())) "
+        $q += ($flt[1].value.split(' ') | foreach-object {
+                "any($(qop "1[9]" "?" $_) $(qop "1[82]" "?" $_))"
+            }) -join ' '
+  
+        $rs = open-celin.ais.script "w4101e" -query $q -returnControlIDs $($global:ctrlIDs["w4101e"])
+        [jdeForm]::update($rs)
+    }
+}
+$ctrlIDs["w4101e"] = "1[123,9,82,113,114]"
+[jdeForm]::types.add(@{ Name = "*W4101E*"; Type = [w4101e] })
+  
