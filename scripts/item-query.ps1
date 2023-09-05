fa29b5438ab84eb464113141814db16c82bfb963
Finnur Bragason <fbragason@outlook.com>
2023-09-06 07:47:39 +0800

    .

--- scripts/item-query.ps1
+++ scripts/item-query.ps1
@@ -1,20 +1,28 @@
 # Init
-
 . getScript qop, jde-form, jde-grid-form, w4101e
     
 set-celin.ais.ui main (getLayout dark)
 $search = convertFrom-celin.ais.ui.form (getLayout item-search)
 $select = convertfrom-celin.ais.ui.gridform (getLayout item-select)
 $pb = convertfrom-celin.ais.ui.progressbar (getLayout pb)
-    
-do {
-    remove-variable flt, item -errorAction silentlycontinue
-    $flt = show-celin.ais.ui.form $search
-    $search.set($flt.data)
-    if ($flt) {
-        show-celin.ais.ui.progressbar { [w4101e]::open($flt.data) } $pb
-        $select.body.data = $jde.grid.detail
-        $item = show-celin.ais.ui.gridform $select
-        $jde.exit()
+
+# Call
+function go {
+    try {
+        do {
+            remove-variable flt, item -errorAction silentlycontinue
+            $flt = show-celin.ais.ui.form $search
+            $search.set($flt.data)
+            if ($flt) {
+                show-celin.ais.ui.progressbar { [w4101e]::open($flt.data) } $pb
+                $select.body.data = $jde.grid.detail
+                $item = show-celin.ais.ui.gridform $select
+                $jde.exit()
+            }
+        } until ($item.key -ne "S")
     }
-} until ($item.key -ne "S")
+    catch {
+        write-host $_
+    }
+    return $item
+}
