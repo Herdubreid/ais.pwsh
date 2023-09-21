4d65999fc5e4310875b13c4c7eb0f915aca9db69
Finnur <fbragason@outlook.com>
2023-09-21 18:57:47 +0800

    .

--- scripts/rec.ps1
+++ scripts/rec.ps1
@@ -1,63 +1,70 @@
 # Init
-. get-script jdeForm, jdeGridForm, w4312f, w4312a
-$pb = convert-celin.ais.ui.progressbar (get-layout pb)
-$errorMsg = convert-celin.ais.ui.prompt (get-layout error-msg)
-$w4312f = convert-celin.ais.ui.gridform (get-layout w4312f)
-$w4312a = convert-celin.ais.ui.gridform (get-layout w4312a)
+. getScript jde-form, jde-grid-form, w4312f, w4312a
 
-try {
+set-celin.ais.ui main (getLayout green)
+set-celin.ais.ui high (getLayout green-high)
 
-  # Receipting Routine
-  $pb.title = "Fetching Open PO's..."
-  show-celin.ais.ui.progressbar { [w4312f]::open() } $pb
-  
-  do {
-    remove-variable sel, rec, opt -errorAction silentlycontinue
+$pb = convertFrom-celin.ais.ui.progressbar (getLayout pb)
+$errorMsg = convertFrom-celin.ais.ui.prompt (getLayout error-msg)
+$w4312f = convertFrom-celin.ais.ui.gridform (getLayout w4312f)
+$w4312a = convertFrom-celin.ais.ui.gridform (getLayout w4312a)
+
+# Receipting Routine
+function go {
+
+  try {
     
-    $w4312f.child.body.data = $jde.grid.detail
-    $sel = show-celin.ais.ui.gridform $w4312f
+    $pb.title = "Fetching Open PO's..."
+    show-celin.ais.ui.progressbar { [w4312f]::open() } $pb
     
-    if ($sel) {
-      $pb.title = "Select Row..."
-      show-celin.ais.ui.progressbar { $jde.select($sel[0].index) } $pb
-    
-      if ($jde.error) {
-        $errorMsg.child.message.text = $jde.rs.error
-        $opt = show-celin.ais.ui.prompt $errorMsg
-      }
-    
-      if ($opt -eq 0) {
-        $pb.title = "Cancel..."
-        show-celin.ais.ui.progressbar { $jde.cancel() } $pb      
-      }
-      else {
-        $w4312a.child.body.data = $jde.grid.detail
-        $rec = show-celin.ais.ui.gridform $w4312a
+    do {
+      remove-variable sel, rec, opt -errorAction silentlycontinue
       
-        if (-not $rec) {
+      $w4312f.body.data = $jde.grid.detail
+      $sel = show-celin.ais.ui.gridform $w4312f
+      
+      if ($sel) {
+        $pb.title = "Select Row..."
+        show-celin.ais.ui.progressbar { $jde.select($sel.data[0].index) } $pb
+        
+        if ($jde.error) {
+          $errorMsg.child.message.text = $jde.rs.error
+          $opt = show-celin.ais.ui.prompt $errorMsg
+        }
+        
+        if ($opt -eq 0) {
           $pb.title = "Cancel..."
-          show-celin.ais.ui.progressbar { $jde.cancel() } $pb
+          show-celin.ais.ui.progressbar { $jde.cancel() } $pb      
         }
         else {
-          $pb.title = "Receipting..."
-          show-celin.ais.ui.progressbar {
-            $jde.receipt($rec[0].index)
-            $jde.ok()
-          } $pb
-          if ($jde.error) {
-            $errorMsg.child.message.text = $jde.rs.error
-            show-celin.ais.ui.prompt $errorMsg
-            $pb.title = "Receipting Failed..."
-            show-celin.ais.ui.progressbar { $jde.cancel() } $pb          
+          $w4312a.body.data = $jde.grid.detail
+          $rec = show-celin.ais.ui.gridform $w4312a
+          
+          if (-not $rec) {
+            $pb.title = "Cancel..."
+            show-celin.ais.ui.progressbar { $jde.cancel() } $pb
           }
           else {
-            $pb.title = "Refreshing Order List..."
-            show-celin.ais.ui.progressbar { $jde.find() } $pb          
+            $pb.title = "Receipting..."
+            show-celin.ais.ui.progressbar {
+              $jde.receipt($rec.data[0].index)
+              $jde.ok()
+            } $pb
+            if ($jde.error) {
+              $errorMsg.message.text = $jde.rs.error
+              show-celin.ais.ui.prompt $errorMsg
+              $pb.title = "Receipting Failed..."
+              show-celin.ais.ui.progressbar { $jde.cancel() } $pb          
+            }
+            else {
+              $pb.title = "Refreshing Order List..."
+              show-celin.ais.ui.progressbar { $jde.find() } $pb          
+            }
           }
         }
       }
-    }
-  } until (-not $sel)
-} catch {
-  trhow $_
+    } until (-not $sel)
+  } catch {
+    write-host $_ -ForegroundColor Red   
+  }
 }
--- scripts/w4312a.ps1
+++ scripts/w4312a.ps1
@@ -2,15 +2,15 @@
 class w4312a : jdeGridForm {
   static [string] $ctrls = "1[382,116,117,103,40,44]"
   [void] receipt([int]$row) {
-    $rs = step-celin.ais.script "update[1 ${row}:(382:1,117: )]" -returnControlIDs ($global:ctrlIDs["w4312a"])
+    $rs = step-celin.ais.script "update[1 ${row}:(382:1,117: )]" -returnControlIDs ([w4312a]::ctrls)
     [jdeForm]::update($rs)
   }
   [void] ok() {
-    $r = step-celin.ais.script "do(4)" -returnControlIDs ([w4312f]::ctrls)
+    $r = step-celin.ais.script "do(4)"
     [jdeForm]::update($r)
   }
   [void] cancel() {
-    $r = step-celin.ais.script "do(5)" -returnControlIDs ([w4312f]::ctrls)
+    $r = step-celin.ais.script "do(5)"
     [jdeForm]::update($r)
   }
   w4312a($rs) : base($rs) {}
