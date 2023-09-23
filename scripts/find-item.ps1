832333e0a801a4fbeb44374347ae07568545f203
Finnur <fbragason@outlook.com>
2023-09-23 12:34:27 +0800

    .

--- scripts/find-item.ps1
+++ scripts/find-item.ps1
@@ -1,23 +1,33 @@
 # Init
+$global:hint = "Item Search & Select (enter 'find' followed by search paramters)"
+
 . getScript qop
 $select = convertfrom-celin.ais.ui.gridform (getLayout item-select)
+$select.many = $true
 
-# Call
+# Find & Select
 function find {
     param(
         [Parameter(ValueFromRemainingArguments=$true)]
         [string[]] $desc
     )
 
-    foreach ($s in $desc) {
-        $q += "any($(qop litm ? $s) $(qop dsc1 ? $s $(qop dsc2 ? $s)))"
+    try {
+
+        foreach ($s in $desc) {
+            $q += "any($(qop litm ? $s) $(qop dsc1 ? $s $(qop dsc2 ? $s)))"
+        }
+        $rs = Submit-Celin.AIS.Query "f4101 (litm,dsc1,dsc2,stkt,lnty) $q"
+        if ($rs.data.grid.detail.length -gt 0) {
+            $select.set($rs.data)
+            $items = Show-Celin.AIS.Ui.GridForm $select
+            $global:items = $items.data | Select-Object Row
+        }
+        else {
+            Write-Output "No matching records!"
+        }
     }
-    $rs = Submit-Celin.AIS.Query "f4101 (litm,dsc1,dsc2,stkt,lnty) $q"
-    if ($rs.data.grid.detail.length -gt 0) {
-        $select.set($rs.data)
-        Show-Celin.AIS.Ui.GridForm $select | Out-Null
-    }
-    else {
-        Write-Output "No matching records!"
+    catch {
+        Write-Host $_ -ForegroundColor Red
     }
 }
