494f241d7e50161294632a64900924db116fd332
Finnur Bragason <fbragason@outlook.com>
2023-09-30 16:34:37 +0800

    .

--- scripts/im/bp.psm1
+++ scripts/im/bp.psm1
@@ -9,13 +9,19 @@ function init {
 
     $bpState.branch = $branch
 
-    $bpState.select = convertfrom-celin.ais.ui.gridform (getLayout base-grid)
-    $bpState.select.many = $true
-    $bpState.select.options = [char[]]@('1')
-    $bpState.select.statusbar.add("Select Items with 1")
-    $bpState.select.body.format = "{0, -15} {1, -30} {2}"
-    $bpState.select.header.format = "{0, -15} {1}"
-    $bpState.select.header.data = @("Item", "Description")
+    $bpState.itms = convertfrom-celin.ais.ui.gridform (getLayout base-grid)
+    $bpState.itms.many = $true
+    $bpState.itms.options = [char[]]@('1')
+    $bpState.itms.statusbar.add("Select Items with 1")
+    $bpState.itms.body.format = "{0, -15} {1, -30} {2}"
+    $bpState.itms.header.format = "{0, -15} {1}"
+    $bpState.itms.header.data = @("Item", "Description")
+
+    $bpState.bps = convertfrom-celin.ais.ui.gridform (getLayout base-grid)
+    $bpState.bps.color = "high"
+    $bpState.bps.body.format = "{4, -15} {1, -30} {2}"
+    $bpState.bps.header.format = "{0, -15} {1}"
+    $bpState.bps.header.data = @("Item", "Description")
 
     $bpState.edit = ConvertFrom-Celin.AIS.Ui.Form (getLayout bp-edit)
 
@@ -29,20 +35,44 @@ function go {
         [string[]] $params
     )
     
-    $options = @("list", "add", "price", "back")
+    $options = @("list", "add", "edit", "back")
+
+    $edit = [System.Func[bool]] {
+        $success = $false
+        Write-Host "Fetching Item/BP List..."
+        try {
+            $bpState.rs = Submit-Celin.AIS.Query "v4101n (f4101.itm,f4102.litm,f4102.mcu,f4101.dsc1,f4101.dsc2) all(f4101.srtx=FLF f4102.mcu=$($bpState.branch))"
+            $bpState.bps.title = "Select $($bpState.branch) Item to Edit"
+            $bpState.bps.set($bpState.rs.data.grid.detail)
+            $bpState.bp = Show-Celin.AIS.Ui.GridForm $bpState.bps
+            if ($bpState.bp) {
+                $q = "all(itm=$($bpState.bp.data.row[0]))"
+                Write-Host $q
+                $bpState.f4102 = Submit-Celin.AIS.Query "f4102 (vend,roqi,safe) $q"
+                $bpState.f4105 = "f4105 (litm,mcu,ledg,uncs,cspo,csin) $q"
+                $bpState.f4106 = "f4106 (litm,mcu,mcu,eftj,exdj,an8,uprc) $q"
+                $success = $true
+            }            
+        }
+        catch {
+            Write-Host $_ -ForegroundColor Red
+        }
+
+        return $success
+    }
 
     $list = [System.Action] {
         Write-Host "Fetching Item/BP List..."
         try {
-            $Global:rs = Submit-Celin.AIS.Query "v4101n (f4102.litm,f4102.mcu,f4101.dsc1,f4101.dsc2) all(f4101.srtx=FLF f4102.mcu=$branch)"
+            $bpState.rs = Submit-Celin.AIS.Query "v4101n (f4101.itm,f4102.litm,f4102.mcu,f4101.dsc1,f4101.dsc2) all(f4101.srtx=FLF f4102.mcu=$($bpState.branch))"
             Write-Host
             [console]::WriteLine("{0, -15} {1, -12} {2, -30} {3}", `
                 @("Item", "Branch", "Description", "Type"))
             [console]::WriteLine("".PadRight(80, '-'))
-            foreach ($r in $rs.data.grid.detail) {
-                [console]::WriteLine("{3,-15} {2, -12} {0, -30} {1}", $r.toarray())
+            foreach ($r in $bpState.rs.data.grid.detail) {
+                [console]::WriteLine("{4,-15} {3, -12} {1, -30} {2}", $r.toarray())
             }
-            Write-Host "$($rs.data.grid.detail.count()) Item(s) returned"
+            Write-Host "$($bpState.rs.data.grid.detail.count()) Item(s) returned"
         }
         catch {
             Write-Host $_ -ForegroundColor Red
@@ -54,7 +84,7 @@ function go {
         $success = $false
         try {
             $bpState.master = Submit-Celin.AIS.Query "f4101 (litm,dsc1,dsc2) by[asc(dsc2)] all(srtx=FLF)"
-            $bpState.bp = Submit-Celin.AIS.Query "v4101n (f4102.litm) all(f4101.srtx=FLF f4102.mcu=$branch)"
+            $bpState.bp = Submit-Celin.AIS.Query "v4101n (f4102.litm) all(f4101.srtx=FLF f4102.mcu=$($bpState.branch))"
             $bpState.available = @()
             foreach ($itm in $bpState.master.data.grid.detail) {
                 $find = $bpState.bp.data.grid.detail | Where-Object { $_[0] -eq $itm[0] }
@@ -63,16 +93,16 @@ function go {
                 }
             }
             if ($bpState.available.Length -gt 0) {
-                $bpState.select.title = "Add Items to Branch $branch"
-                $bpState.select.set($available)
-                $bpState.toAdd = Show-Celin.AIS.Ui.GridForm $bpState.select
+                $bpState.itms.title = "Add Items to Branch $($bpState.branch)"
+                $bpState.itms.set($bpState.available)
+                $bpState.toAdd = Show-Celin.AIS.Ui.GridForm $bpState.itms
                 if ($bpState.toAdd) {
                     if ($bpState.toAdd.data.toarray().length -gt 0) {
                         foreach ($item in $bpState.toAdd.data) {
                             try {
                                 Write-Host "Adding $($item.row[0]) $($item.row[1])"
                                 $bpState.rs = open-celin.ais.script "w41026e zjde0001" "do(47)" -returnControlIDs $bpState.rc
-                                $bpState.rs = step-celin.ais.script "set(15,$branch) set(16,$($item.row[0])) set(145,3104) set(151,4242) do(11)" -returnControlIDs $bpState.rc
+                                $bpState.rs = step-celin.ais.script "set(15,$($bpState.branch)) set(16,$($item.row[0])) set(145,3104) set(151,4242) do(11)" -returnControlIDs $bpState.rc
                                 if ($bpState.rs.error) {
                                     throw $bpState.rs.error
                                 }
@@ -116,7 +146,12 @@ function go {
             }
         }
         $options[2] {
-
+            if ($edit.Invoke()) {
+                Write-Host "Edit Successful." -ForegroundColor Green
+            }
+            else {
+                Write-Host "Edit Cancelled" -ForegroundColor Red
+            }            
         }
         $options[3] {
             $Global:mod = $null
