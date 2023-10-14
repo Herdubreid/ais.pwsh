a2e0088ef5cb8cdf2f75e39100e60a06877b7d23
Finnur Bragason <fbragason@outlook.com>
2023-10-14 14:37:29 +0800

    .

---
+++ scripts/sum-ac.psm1
@@ -0,0 +1,50 @@
+function sumAc {
+    param (
+        $data
+    )
+    $rows = @()
+    $f0911 = "f0911 [group(crcd) sum(aa) count(aa)]"
+
+    for ($cnt = 0; $cnt -lt $data.count(); $cnt++) {
+        $r1 = $data[$cnt]
+        $mcuQ = "fy=23 lt=AA post=P mcu=$($r1[2].trim())"
+        $q = "all($mcuQ"
+        if ($cnt -lt $data.count() - 1) {
+            $q += " obj=$($r1[3].trim())"
+            if ($r1[4].trim()) {
+                $q += " sub>=$($r1[4].trim())"
+            }
+            $r2 = $data[$cnt + 1]
+            $q += ") or all($mcuQ obj>$($r1[3].trim()) obj<$($r2[3].trim())"
+            if ($r2[4].trim()) {
+                $q += ") or all($mcuQ obj=$($r2[3].trim()) sub<$($r2[4].trim())"
+            }
+        }
+        else {
+            $q += " obj>=$($r1[3].trim())"
+            if ($r1[4].trim()) {
+                $q += " sub>=$($r1[4].trim())"
+            }
+        }
+        write-host "$f0911 $q)"
+        write-host
+        try {
+            $rs = Submit-Celin.AIS.Query "$f0911 $q)"
+            if ($rs.data.grid.detail.count() -gt 0) {
+                foreach ($r in $rs.data.grid.detail) {
+                    $row = $data[$cnt] + $r
+                    $rows += , @($row)
+                }
+            }
+            else {
+                $row = $data[$cnt] + @($null, $null, $null)
+                $rows += , @($row)
+            }
+        }
+        catch {
+            Write-Host $_ -ForegroundColor Red
+            return $rows
+        }
+    }
+    return $rows
+}
---
+++ scripts/test2.ps1
@@ -0,0 +1,70 @@
+# Init
+$ac.f0901 = "f0901 -max no (co,aid,mcu,obj,sub,dl01,lda,pec,crcd) by[asc(obj,sub)]"
+$ac.fmt = { , @(("{0}.{1}.{2}" -f $_[2].trim(), $_[3], $_[4].trim()).trimEnd('.'), $_[10], $_[9], $_[5], $_[11], $_[6], $_[7]) }
+# Get Msg Form
+$ac.msg = ConvertFrom-Celin.AIS.Ui.Prompt (getLayout error-msg)
+# Get a base Grid Form
+$ac.accFrm = ConvertFrom-Celin.AIS.Ui.GridForm (getLayout base-grid)
+# Set Title and Color
+$ac.accFrm.title = "Accounts"
+$ac.accFrm.color = "high"
+# Set Keys
+$ac.accFrm.statusbar.add("Esc - Back to BU's")
+$ac.accFrm.statusbar.add("To Excel", "Alt-X")
+# Set the Header
+$ac.accFrm.header.format = "{0,20} {1, 17:N2} {2,3} {3,-30} {4,3} {5,3} {6,3}"
+$ac.accFrm.header.data = @("Account", "Balance", "CUR", "Description", "Trx", "LOD", "PEC")
+# Set the Body
+$ac.accFrm.body.format = "{0,20} {1, 17:N2} {2,3} {3,-30} {4,3} {5,3} {6,3}"
+try {
+    # Loop until Esc
+    do {
+        # Show Form
+        $ac.mcu = Show-Celin.AIS.Ui.GridForm $ac.mcuFrm
+        if ($ac.mcu) {
+            $ac.mcuq = "mcu=$($ac.mcu.data.row[0].trim())"
+            # Fetch the First Level
+            $ac.lod = Submit-Celin.AIS.Query "$($ac.f0901) all($($ac.mcuq) lda=$($ac.mcu.data.row[7]) obj!blank)"
+            # Sum the Level
+            $ac.lodRows = sumAc $ac.lod.data.grid.detail
+            $ac.rows = @($ac.lodRows | foreach-object $ac.fmt) 
+            $ac.accFrm.set($ac.rows)
+            # Loop until Esc
+            do {
+                # Show Form
+                $ac.next = Show-Celin.AIS.Ui.GridForm $ac.accFrm
+                if ($ac.next) {
+                    # Get the Original Row
+                    $ac.r = $ac.lodRows[$ac.next.data.index]
+                    # Build the Query String
+                    $ac.q = "$($ac.mcuq) obj>=$($ac.r[3].trim())"
+                    if ($ac.r[4].trim()) {
+                        $ac.q += " sub>=$($ac.r[4].trim())"
+                    }
+                    if ($ac.next.data.index -lt $ac.lod.data.grid.detail.count() - 1) {
+                        $ac.r = $ac.lodRows[$ac.next.data.index + 1]
+                        $ac.q += " obj<$($ac.r[3].trim())"
+                        if ($ac.r[4].trim()) {
+                            $ac.q += " sub<$($ac.r[4].trim())"
+                        }
+                    }
+                    $rs = Submit-Celin.AIS.Query "$($ac.f0901) all(lda=$([int]$ac.r[6] + 1) $($ac.q))"
+                    if ($rs.data.grid.detail.count() -gt 0) {
+                        $ac.lod = $rs
+                        # Sum the Level
+                        $ac.lodRows = sumAc $ac.lod.data.grid.detail
+                        $ac.rows = @($ac.lodRows | foreach-object $ac.fmt) 
+                        $ac.accFrm.set($ac.rows)
+                    }
+                    else {
+                        $ac.msg.Message.Text = "No records returned!"
+                        Show-Celin.AIS.Ui.Prompt $ac.msg | Out-Null
+                    }
+                }
+            } until (-not $ac.next)
+        }
+    } until (-not $ac.mcu)
+}
+catch {
+    Write-Host $_ -ForegroundColor Red
+}
