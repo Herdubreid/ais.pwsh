ad699e196ebc13d45bf206c6701dc051f4452fd9
Finnur Bragason <fbragason@outlook.com>
2023-10-27 10:24:02 +0800

    .

--- scripts/frm/default.psm1
+++ scripts/frm/default.psm1
@@ -1,35 +1,49 @@
 function init {
-    $global:hint = "Form Query (enter 'go' to start, 'q' to quit)"
+    # Set Constants
+    New-Variable search (convertFrom-celin.ais.ui.form (getLayout form-search)) -Option ReadOnly -Force -Scope Global
+    New-Variable select (convertfrom-celin.ais.ui.gridform (getLayout form-select)) -Option ReadOnly -Force -Scope Global
+    New-Variable pb (ConvertFrom-celin.ais.ui.progressbar (getLayout pb)) -Option ReadOnly -Force -Scope Global
+    
+    # Helpers
     getScript qop
-
-    $global:frm = @{}
-    $frm.search = convertFrom-celin.ais.ui.form (getLayout form-search)
-    $frm.select = convertfrom-celin.ais.ui.gridform (getLayout form-select)
-    $frm.pb = ConvertFrom-celin.ais.ui.progressbar (getLayout pb)
 }
 
 function go {
+    # Variables
+    $var = New-Celin.State frm filter, cql, rs, form, demo -Force
+    
     try {
-        $flt = show-celin.ais.ui.form $frm.search
-        if ($flt) {
-            $s = "f9865 (fmnm,md,fmpt,sy) all($(qop "fmnm" "^" $flt.data[0].value.toupper()) $(qop "md" "?" $flt.data[1].value) $(qop "fmpt" "=" $flt.data[2].value.toupper()) $(qop "sy" "^" $flt.data[3].value)".trim() + ")"
-            
-            show-celin.ais.ui.progressbar { $frm.rs = submit-celin.ais.query $s } $frm.pb
-            
-            $frm.select.set($frm.rs.data)
-            $form = show-celin.ais.ui.gridform $frm.select
-            if ($form) {
-                $frm.demo = get-celin.ais.ui $form.data[0].row[0]
+        while ($true) {
+            # Prompt for Filter Paramters
+            Show-Celin.AIS.Ui.Form $search | cset filter
+            if ($var.values.filter) {
+                $v = $var.value.filter.data
+                # Build the cql statement (this looks a bit ugly)
+                cset cql "f9865 (fmnm,md,fmpt,sy) all($(qop fmnm^ $v[0].value.toupper())$(qop md? $v[1].value)$(qop fmpt= $v[2].value.toupper())$(qop sy^ $v[3].value))"
+                # Submit the query with Progress Bar
+                Show-Celin.AIS.Ui.ProgressBar {
+                    submit-celin.ais.query $var.value.cql | cset rs
+                } $pb
+                # Select a Form
+                $select.set($var.value.rs.data)
+                Show-Celin.AIS.Ui.GridForm $select | cset form
+                if ($var.value.form) {
+                    # Get the Demo def
+                    Get-Celin.AIS.Ui $var.value.form.data[0].row[0] | cset demo
+                    # Label the result with the Form name (clear and force override, 4th parameter)
+                    $var.setLabel($var.value.form.data[0].row[0], 0, $true, $true)
+                }
+                else {
+                    # Labe the state as done
+                    $var.setLabel("done", 0, $true, $true)
+                }
+            }
+            else {
+                break
             }
         }
     }
     catch {
-        write-host $_ -ForegroundColor Red
+        Write-Host $_ -ForegroundColor Red
     }
-}
-
-function q {
-    Remove-Item function:\go
-    $global:hint = $null
-    Clear-Host
 }
