3eaaf887bfa1d36d3383fd99dd4a86c56ed69d8a
Finnur Bragason <fbragason@outlook.com>
2023-12-05 10:25:03 +0800

    .

--- scripts/frm/default.psm1
+++ scripts/frm/default.psm1
@@ -5,6 +5,8 @@ function init {
     New-Variable search (convertFrom-celin.ais.ui.form (getLayout form-search)) -Option ReadOnly -Force -Scope Global
     New-Variable select (convertfrom-celin.ais.ui.gridform (getLayout form-select)) -Option ReadOnly -Force -Scope Global
     New-Variable pb (ConvertFrom-celin.ais.ui.progressbar (getLayout pb)) -Option ReadOnly -Force -Scope Global
+    New-Variable prompt (ConvertFrom-Celin.AIS.Ui.Prompt (getLayout error-msg)) -Option ReadOnly -Force -Scope Global
+    $prompt.message.text = "No Forms found!"
     
     # Helpers
     getScript qop
@@ -17,27 +19,32 @@ function go {
     try {
         while ($true) {
             # Prompt for Filter Paramters
-            Show-Celin.AIS.Ui.Form $search | cset filter
-            if ($var.values.filter) {
-                $v = $var.value.filter.data
+            Show-Celin.AIS.Ui.Form $search | cstate filter -FalseIfNull
+            if ($var.filter) {
+                $v = $var.filter.data
                 # Build the cql statement (this looks a bit ugly)
-                cset cql "f9865 (fmnm,md,fmpt,sy) all($(qop fmnm^ $v[0].value.toupper())$(qop md? $v[1].value)$(qop fmpt= $v[2].value.toupper())$(qop sy^ $v[3].value))"
+                cstate cql "f9865 (fmnm,md,fmpt,sy) all($(qop fmnm^ $v[0].value.toupper())$(qop md? $v[1].value)$(qop fmpt= $v[2].value.toupper())$(qop sy^ $v[3].value))"
                 # Submit the query with Progress Bar
                 Show-Celin.AIS.Ui.ProgressBar {
-                    submit-celin.ais.query $var.value.cql | cset rs
+                    submit-celin.ais.query $var.cql | cstate rs
                 } $pb
-                # Select a Form
-                $select.set($var.value.rs.data)
-                Show-Celin.AIS.Ui.GridForm $select | cset form
-                if ($var.value.form) {
-                    # Get the Demo def
-                    Get-Celin.AIS.Ui $var.value.form.data[0].row[0] | cset demo
-                    # Label the result with the Form name (clear and force override, 4th parameter)
-                    $var.setLabel($var.value.form.data[0].row[0], 0, $true, $true)
-                }
-                else {
-                    # Labe the state as done
-                    $var.setLabel("done", 0, $true, $true)
+
+                if ($var.rs.data.grid.detail.length -gt 0) {
+                    # Select a Form
+                    $select.set($var.rs.data)
+                    Show-Celin.AIS.Ui.GridForm $select | cstate form -FalseIfNull
+                    if ($var.form) {
+                        # Get the Demo def
+                        Get-Celin.AIS.Ui $var.form.data[0].row[0] | cstate demo
+                        # Label the result with the Form name (clear and force override, 4th parameter)
+                        $var.setLabel($var.form.data[0].row[0], $true, $true)
+                    }
+                    else {
+                        # Labe the state as done
+                        $var.setLabel("done", 0, $true, $true)
+                    }
+                } else {
+                    show-celin.ais.ui.prompt $prompt | Out-Null
                 }
             }
             else {
