7eed4e9666958754bc0c08537c69b0b8c1d7d0b3
Finnur Bragason <fbragason@outlook.com>
2023-12-10 16:20:03 +0800

    .

--- scripts/demo.ps1
+++ scripts/demo.ps1
@@ -1,3 +1,4 @@
+
 # Fetch PO's for P01012
 go P01012
 # Get the State Variable
@@ -5,3 +6,19 @@ $po = Use-Celin.State po
 # The PO for each Version has a label, so we can collect them
 # in one go and export to excel
 $po.labels.po | Export-Excel
+
+# Fetch Specs for R42565
+go r42565
+# Get the State Variable
+$po = Use-Celin.State po
+# Get the Tab Page's Controls with the Fields we want
+# We only need this once
+$controls = $po.po.data.poprompt.tabpages.controls | Select-Object pageNumber, title, memberName, idObject
+# Get the PO values for each version
+$pos = $po.labels.po.data | Select-Object reportVersion, poValues
+# Define PoValue lookup
+$poLookup = { param($id) $pos | ForEach-Object {@{ $_.reportVersion = $_.poValues.where({$_.id -eq $id }).value }}}
+# Add version PO values
+$vers = $controls | ForEach-Object { cpo $_, (cpo $poLookup.Invoke($_.idObject)) }
+# Export to Excel
+$vers | Export-Excel
--- scripts/po/default.psm1
+++ scripts/po/default.psm1
@@ -1,32 +1,41 @@
 function init {
-    $global:hint = "App PO's.  Enter 'go <app>'"
+    $global:hint = "App/Ube PO's.  Enter 'go <object name>'"
 }
 
 function go {
     param (
         [Parameter(Mandatory = $true)]
-        [string] $app        
+        [string] $obnm        
     )
-    $po = New-Celin.State po app, vers, po, rs -Force
+    $po = New-Celin.State po obnm, vers, po, rs -Force
 
     try {
-        cstate app $app.ToUpper()
+        cstate obnm $obnm.ToUpper()
         # Fetch Versions
-        Submit-Celin.AIS.Query "f983051 (pid, vers, jd) all(pid=$($po.app))" | cstate rs
+        Submit-Celin.AIS.Query "f983051 (pid, vers, jd) all(pid=$($po.obnm))" | cstate rs
         if ($po.rs.data.grid.detail.length -gt 0) {
             foreach ($ver in $po.rs.data.grid.detail) {
                 [System.Console]::Write("{0}|{1}`r", $ver[0], $ver[1])
                 # Fetch the Version
                 cstate vers $ver[1]
-                Get-Celin.AIS.Po $po.app $po.vers -WithApp | cstate po
+                if ($po.obnm.StartsWith('R')) {
+                    # Ube
+                    Get-Celin.AIS.Ube $po.obnm $po.vers | cstate po
+                }
+                else {
+                    # App
+                    Get-Celin.AIS.Po $po.obnm $po.vers -WithApp | cstate po
+                }
                 # Label the version
-                $po.setLabel("$($po.app)|$($po.vers)", $false, $true) | Out-Null
+                $po.setLabel("$($po.obnm)|$($po.vers)", $false, $true) | Out-Null
             }
             [System.Console]::WriteLine("`r");
-        } else {
-            Write-Host "No versions for $($po.app)" -ForegroundColor Red
         }
-    } catch {
+        else {
+            Write-Host "No versions for $($po.obnm)" -ForegroundColor Red
+        }
+    }
+    catch {
         # Report error to the console
         throw
     }
